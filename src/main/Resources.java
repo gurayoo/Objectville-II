fc3c0890112fb7a916096c1632b27810a1487adc
gurayoo <ozturk.guray07@gmail.com>
2026-05-23 20:28:35 +0300

    Update Resources.java
    commit of resources

--- src/main/Resources.java
+++ src/main/Resources.java
@@ -1,4 +1,38 @@
 package main;
 
 public class Resources {
+    //resources will be taken from this class
+    private int population;
+    private int goods;
+    private int lifestyle;
+
+    public Resources(int goods, int lifestyle, int population) {
+        this.goods = goods;
+        this.lifestyle = lifestyle;
+        this.population = population;
+    }
+
+    public int getGoods() {
+        return goods;
+    }
+
+    public void setGoods(int goods) {
+        this.goods = goods;
+    }
+
+    public int getLifestyle() {
+        return lifestyle;
+    }
+
+    public void setLifestyle(int lifestyle) {
+        this.lifestyle = lifestyle;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
