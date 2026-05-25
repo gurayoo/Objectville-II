c3cffbe116d9208a2b0b57fb993eac61cd04f95d
efeunal35 <a.efe.unal@gmail.com>
2026-05-25 16:21:37 +0300

    Industrial added
    commit of industrial

--- src/cells/zones/Industrial.java
+++ src/cells/zones/Industrial.java
@@ -1,4 +1,46 @@
 package cells.zones;
 
-public class Industrial {
+import main.Resources;
+
+public class Industrial extends Zone{
+    @Override
+    public int getM() {  //specific rule of getm in industrial
+        return Math.min(getWater(), getElectricity());
+    }
+
+    @Override
+    public void calculateLevel() { //
+    int minlevel =0;
+    if (getElectricity() > 0 && getWater() > 0 && resources.getPopulation() > 0){
+        minlevel++;
+        if (isHasSecurity() == true){
+            minlevel++;
+
+            if (resources.getPopulation() > getCurrentOutput()){
+                minlevel++;
+            }
+        }
+    } if (minlevel > getLevel()) setLevel(getLevel() +1); //guaranties increase level only 1 per tick
+    else if (minlevel < getLevel()) setLevel(getLevel() -1);//guaranties decrease level only 1 per tick
+
+    }
+
+    @Override
+    public void calculateOutput(int a) { // specified for industrial
+    if (getLevel() == 0) setCurrentOutput(0);
+    if (getLevel() == 1) setCurrentOutput(a);
+    if (getLevel() == 2) setCurrentOutput(2*a);
+    if (getLevel() == 3) setCurrentOutput((2*a)+ resources.getPopulation());
+    }
+
+    @Override
+    public char getChar() {
+        return 'I';
+    }
+
+
+
+
+
+
 }
