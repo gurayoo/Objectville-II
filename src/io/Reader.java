8048362e6f19b154c86bf00fcab8dcaf47774064
gurayoo <ozturk.guray07@gmail.com>
2026-05-26 18:06:20 +0300

    Reader class

--- src/io/Reader.java
+++ src/io/Reader.java
@@ -1,4 +1,70 @@
 package io;
 
+import cells.Cell;
+import cells.EmptyCell;
+import cells.Road;
+import cells.services.Hospital;
+import cells.services.PoliceStation;
+import cells.services.School;
+import cells.utilityProviders.InternetHub;
+import cells.utilityProviders.PowerPlant;
+import cells.utilityProviders.WaterPumpingStation;
+import cells.zones.Commercial;
+import cells.zones.Housing;
+import cells.zones.Industrial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Reader {
+    public static Cell[][] loadMap(String filePath) {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            List<String> list = reader.readAllLines();
+            if (list.isEmpty()) {
+                throw new IOException("ERROR!!!EMPTY LIST!!!");
+            }
+            int row = list.size();
+            int column = list.get(0).length();
+            Cell[][] map = new Cell[row][column];
+
+            for (int a = 0; a < row; a++) {
+                String line = list.get(a);
+                for (int b = 0; b < column; b++) {
+                    char c = line.charAt(b);
+                    if (c == 'H') {
+                        map[a][b] = new Housing();
+                    } else if (c == 'I') {
+                        map[a][b] = new Industrial();
+                    } else if (c == 'C') {
+                        map[a][b] = new Commercial();
+                    } else if (c == 'P') {
+                        map[a][b] = new PowerPlant();
+                    } else if (c == 'W') {
+                        map[a][b] = new WaterPumpingStation();
+                    } else if (c == 'T') {
+                        map[a][b] = new InternetHub();
+                    } else if (c == 'F') {
+                        map[a][b] = new PoliceStation();
+                    } else if (c == 'D') {
+                        map[a][b] = new Hospital();
+                    } else if (c == 'S') {
+                        map[a][b] = new School();
+                    } else if (c == 'R') {
+                        map[a][b] = new Road();
+                    } else if (c == 'E') {
+                        map[a][b] = new EmptyCell();
+                    } else {
+                        throw new IllegalArgumentException("THERE İS WRONG TYPE OF CHARACTER IN THE MAP!!!");
+                    }
+
+                }
+            }
+            return map;
+        } catch (IOException e) {
+            throw new RuntimeException("ERROR!!!" + e.getMessage());
+        }
+    }
 }
